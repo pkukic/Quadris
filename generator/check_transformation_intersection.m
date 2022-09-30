020f5d7c0c2f94df09d6ea5dcd8a20f782782145
Patrik Kukic <patrikkukic@gmail.com>
2022-09-30 15:17:25 +0200

    comments

--- generator/check_transformation_intersection.m
+++ generator/check_transformation_intersection.m
@@ -1,12 +1,14 @@
 function [flag] = check_transformation_intersection(mat,data)
 [ro1, ro2, ro3, ro4, mu1, mu2, delta1, delta2] = reflections_and_symmetries(mat);
+% Concatenate along the third axis
 s = cat(3, ro1, ro2, ro3, ro4, mu1, mu2, delta1, delta2);
 if isempty(data)
     flag = 0;
     return
 else
     [m, n, p] = size(data);
-    [m, n, q ] = size(s);
+    [m, n, q] = size(s);
+    % Get intersection
     result = intersect(reshape(s, [], q).', reshape(data, [], p).', 'rows');
     result = reshape(result.', m, n, []);
     flag = ~isempty(result);
